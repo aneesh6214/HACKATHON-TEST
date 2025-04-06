fee3878ed5d65d017efef5c1e11495acde80a832
Aneesh <122716157+aneesh6214@users.noreply.github.com>
2025-04-05 18:00:22 -0700

    h-screen

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -108,7 +108,8 @@ export default function Home() {
 
       <main className="flex-1 relative z-10">
         {/* Welcome Section */}
-        <section id="welcome" className="w-full h-[650px] md:h-[650px] lg:h-[650px] pt-32">
+        {/* <section id="welcome" className="w-full h-[650px] md:h-[650px] lg:h-[650px] pt-32"> */}
+        <section id="welcome" className="w-full h-screen pt-32">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
               <div className="flex flex-col justify-center space-y-4">
